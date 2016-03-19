bdcc25489d145e4ee261d017f728ffe1117e36f5
MapuH <m.s.marinov@gmail.com>
2016-03-19 22:46:53 +0200

    added exam Java Fundamentals Retake 26/10/2015

---
+++ src/javaBasics26Oct2015/TinySporebat.java
@@ -0,0 +1,54 @@
+    package javaBasics26Oct2015;
+
+    import java.util.*;
+
+    /*
+     * Created by MapuH on 19-Mar-2016
+     *
+     * Description, solution and test can be found at:
+     * https://softuni.bg/downloads/svn/java-basics/Exams/2015-10-26/Problem01-Tiny-Sporebat.zip
+     */
+    public class TinySporebat {
+
+        public static void main(String[] args) {
+
+            Scanner input = new Scanner(System.in);
+            String line = input.nextLine().trim();
+
+            int health = 5800;
+            int glowcaps = 0;
+            boolean isDead = false;
+
+            journey:
+            while (!line.equals("Sporeggar")) {
+                char[] enemies = line.substring(0, line.length()-1).toCharArray();
+
+                for (char enemy : enemies) {
+                    if (enemy == 'L') {
+                        health += 200;
+                    } else {
+                        health -= enemy;
+                    }
+                    if (health <= 0) {
+                        isDead = true;
+                        break journey;
+                    }
+                }
+
+                glowcaps += Integer.parseInt(line.substring(line.length()-1));
+                line = input.nextLine().trim();
+            }
+
+            if (isDead) {
+                System.out.println("Died. Glowcaps: " + glowcaps);
+            } else {
+                System.out.println("Health left: " + health);
+
+                if (glowcaps >= 30) {
+                    System.out.println("Bought the sporebat. Glowcaps left: " + glowcaps % 30);
+                } else {
+                    System.out.printf("Safe in Sporeggar, but another %d Glowcaps needed.\n", 30 - glowcaps);
+                }
+            }
+        }
+    }
---
+++ src/javaBasics26Oct2015/WeirdScript.java
@@ -0,0 +1,51 @@
+package javaBasics26Oct2015;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/*
+ * Created by MapuH on 19-Mar-2016
+ *
+ * Description, solution and test can be found at:
+ * https://softuni.bg/downloads/svn/java-basics/Exams/2015-10-26/Problem02-Weird-Script.zip
+ */
+public class WeirdScript {
+
+    private static String findKeyLetters(int key) {
+        String letter;
+        int value = key % 52;
+        if (value == 0) {
+            letter = String.valueOf((char) 90);
+        } else if (value < 27) {
+            letter = String.valueOf((char) (value + 96));
+        } else {
+            letter = String.valueOf((char) (value + 38));
+        }
+        return letter + letter;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        int key = Integer.parseInt(input.nextLine());
+        String keyStr = findKeyLetters(key);
+        String text = "";
+        String line = input.nextLine().trim();
+
+        while (!line.equals("End")) {
+            text += line;
+            line = input.nextLine().trim();
+        }
+
+        Pattern pattern = Pattern.compile(keyStr + ".*?" + keyStr);
+        Matcher matcher = pattern.matcher(text);
+        while (matcher.find()) {
+            String output = matcher.group(0).replaceAll(keyStr, "");
+            if (!output.equals("")) {
+                System.out.println(output);
+            }
+        }
+
+    }
+}
